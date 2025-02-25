459c82b84de9f7ac175a930740e718f96ed74123
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-25 16:15:26 +0800

    First version with implementation, testing

--- src/rx_fitter/components.py
+++ src/rx_fitter/components.py
@@ -1,17 +1,27 @@
 '''
 Module with functions needed to provide fit components
 '''
+# pylint: disable=too-many-positional-arguments
+
+import os
 import copy
 
+from ROOT                                        import RDataFrame
+from dmu.stats.model_factory                     import ModelFactory
+from dmu.logging.log_store                       import LogStore
+from rx_selection                                import selection as sel
+from rx_data.rdf_getter                          import RDFGetter
 from rx_calibration.hltcalibration.fit_component import FitComponent
 from rx_fitter.prec                              import PRec
 
+log = LogStore.add_logger('rx_fitter:components')
 # ------------------------------------
 class Data:
     '''
     Data class
     '''
-    cfg = {
+    cache_dir = '/home/acampove/Data/RX_run3/cache/rx_fits'
+    cfg       = {
             'out_dir': 'plots/fit',
             'fitting':
             {
@@ -26,31 +36,63 @@ class Data:
                 'stacked' : True,
                 },
             }
-# ------------------------------------
-def get_mc(name : str, q2bin : str, model : list[str]) -> FitComponent:
-    wp_name        = _get_wp_name()
-    cfg            = copy.deepcopy(Data.cfg)
-    cfg['name']    = name
-    out_dir        = cfg['out_dir']
-    cfg['out_dir'] = f'{out_dir}/{q2_bin}/{name}/{wp_name}'
+# ---------------------------------
+def _get_rdf(sample : str, q2bin : str, trigger : str) -> RDataFrame:
+    out_path = f'{Data.cache_dir}/{sample}_{q2bin}.root'
+    if os.path.isfile(out_path):
+        log.info('DataFrame already cached, reloading')
+        rdf = RDataFrame('tree', out_path)
+        return rdf
 
-    rdf   = _get_rdf(sample)
+    log.info('DataFrame not cached')
+
+    gtr = RDFGetter(sample=sample, trigger=trigger)
+    rdf = gtr.get_rdf()
+
+    analysis = 'MM' if 'MuMu' in trigger else 'EE'
+
+    d_sel = sel.selection(project='RK', analysis=analysis, q2bin=q2bin, process=sample)
+    for cut_name, cut_value in d_sel.items():
+        log.info(f'{cut_name:<20}{cut_value}')
+        rdf = rdf.Filter(cut_value, cut_name)
+
+    return rdf
+# ------------------------------------
+def get_mc(obs, sample : str, q2bin : str, trigger : str, model : list[str]) -> FitComponent:
+    '''
+    Will return FitComponent object for given MC sample
+    '''
+    cfg            = copy.deepcopy(Data.cfg)
+    cfg['name']    = sample
+    out_dir        = cfg['out_dir']
+    cfg['out_dir'] = f'{out_dir}/{q2bin}/{sample}_{trigger}'
+
+    rdf   = _get_rdf(sample, q2bin, trigger)
     rdf   = rdf.Define('weights', '1')
 
-    l_pdf, l_shr = _get_fitting_model(sample)
-    if l_pdf == ['kde']:
-        pdf = None
-    else:
-        mod   = ModelFactory(preffix=sample, obs = Data.obs, l_pdf = l_pdf, l_shared=l_shr)
-        pdf   = mod.get_pdf()
+    mod   = ModelFactory(preffix=sample, obs = obs, l_pdf = model, l_shared=['mu', 'sg'])
+    pdf   = mod.get_pdf()
 
-    obj   = FitComponent(cfg=cfg, rdf=rdf, pdf=pdf, obs=Data.obs)
+    obj   = FitComponent(cfg=cfg, rdf=rdf, pdf=pdf, obs=obs)
+
+    return obj
 # ------------------------------------
-def get_prc(channel : str, is_dtf : bool) -> FitComponent:
+def get_prc(obs, q2bin : str, trigger : str) -> FitComponent:
     '''
-    Function returning FitComponent object for Partially reconstructed background 
+    Function returning FitComponent object for Partially reconstructed background
+    '''
+    mass   = obs.obs[0]
+    cfg    = copy.deepcopy(Data.cfg)
+    bw     = {'jpsi' :  5, 'psi2' : 10}[q2bin]
+    l_samp = [
+            'Bu_JpsiX_ee_eq_JpsiInAcc',
+            'Bd_JpsiX_ee_eq_JpsiInAcc',
+            'Bs_JpsiX_ee_eq_JpsiInAcc']
 
-    channel (str): Electron (ee) or muon (mm) channel
-    is_dtf  (bool): Signals if the component was obtaining by constraining the dilepton to the J/psi mass
-    '''
-    
+    d_wgt= {'dec' : 1, 'sam' : 1}
+    obj=PRec(samples=l_samp, trig=trigger, q2bin=q2bin, d_weight=d_wgt)
+    pdf=obj.get_sum(mass=mass, name='PRec', obs=obs, bandwidth=bw)
+    fcm= FitComponent(cfg=cfg, rdf=None, pdf=pdf)
+
+    return fcm
+# ------------------------------------
