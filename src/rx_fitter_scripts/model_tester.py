f7bd5200e1cb1cdaa0155813bdc93ee60b4283c7
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-26 12:46:40 +0800

    Adding file

---
+++ src/rx_fitter_scripts/model_tester.py
@@ -0,0 +1,22 @@
+'''
+Script used to test PDFs for fits to MC
+'''
+
+from rx_fitter import components as cmp
+
+# -----------------------------
+class Data:
+    '''
+    Data class
+    '''
+
+# -----------------------------
+def main():
+    '''
+    Starts here
+    '''
+    _parse_args()
+
+# -----------------------------
+if __name__ == '__main__':
+    main()
