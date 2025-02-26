c9c2bcafeb1158d3729a49707e9ddb1b96c4c0cc
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-26 13:20:45 +0800

    WIP

--- tests/test_components.py
+++ tests/test_components.py
@@ -22,9 +22,35 @@ def test_mc():
     '''
     Testing creation of PDF from MC sample
     '''
-    obs=zfit.Space('B_const_mass_M', limits=(4500, 6000))
+    #obs=zfit.Space('B_const_mass_M', limits=(5100, 5500))
+    obs=zfit.Space('B_M', limits=(4500, 6000))
     trigger = 'Hlt2RD_BuToKpEE_MVA'
 
-    cmp_sig = cmp.get_mc(obs = obs, sample = 'Bu_JpsiK_ee_eq_DPC' , trigger = trigger, q2bin='jpsi', model=['cbl', 'cbl', 'cbr'])
+    cmp_sig = cmp.get_mc(obs    = obs,
+                         sample = 'Bu_JpsiK_ee_eq_DPC',
+                         trigger= trigger,
+                         q2bin  = 'jpsi',
+
+                         #model  = ['cbl'])
+                         model  = ['cbl', 'cbr'])
+                         #model  =['dscb'])
+                         #model  = ['suj'])
+
+                         #model  =['dscb']) # works with brem == 2
+                         #model  = ['cbl', 'cbr', 'dscb'])
+                         #model  = ['cbl', 'cbr'])
+                         #model  = ['dscb', 'cbl'])
+                         #model  =['cbl', 'cbl', 'cbr'])
+                         #model  =['dscb', 'dscb'])
+
+                         # DTF mass
+                         #model  =['cbl', 'dscb', 'cbl'])
+
+                         #model  =['cbl', 'cbl', 'cbr'])
+                         #model  =['dscb', 'dscb'])
+                         #model  =['dscb', 'dscb', 'dscb'])
+                         #model  =['dscb']) # works with brem == 2
+                         #model  =['dscb']) # works with brem == 1
+                         #model  =['dscb']) # works with brem == 0
     cmp_sig.run()
 # --------------------------------------------------------------
