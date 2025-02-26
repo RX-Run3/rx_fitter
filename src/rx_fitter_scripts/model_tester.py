1db0a60a02d30cf7ba6cbab361c99b31f3f466ad
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-26 13:20:08 +0800

    Bug: Specify correctly output directory

--- src/rx_fitter_scripts/model_tester.py
+++ src/rx_fitter_scripts/model_tester.py
@@ -57,10 +57,10 @@ def main():
     _parse_args()
     _load_config()
 
-    obs                 = _get_obs()
-    l_mod               = Data.cfg['models'][Data.model]
-    RDFGetter.samples   = Data.cfg['samples']
-    cmp.Data.cfg.out_dir= Data.cfg['out_dir']
+    obs                     = _get_obs()
+    l_mod                   = Data.cfg['models'][Data.model]
+    RDFGetter.samples       = Data.cfg['samples']
+    cmp.Data.cfg['out_dir'] = Data.cfg['out_dir']
 
     cmp_sig = cmp.get_mc(obs    = obs,
                          sample = Data.sample,
