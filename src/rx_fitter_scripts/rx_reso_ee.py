d1916702f037701131ca8cdf4b0c02ca7728fcf7
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-27 19:54:54 +0800

    Replacing range with kind arg
    Can be used to pick fitting range and selection

--- src/rx_fitter_scripts/rx_reso_ee.py
+++ src/rx_fitter_scripts/rx_reso_ee.py
@@ -18,7 +18,9 @@ class Data:
     '''
     nbrem : int
     mass  : str
-    rng   : str
+    kind  : str
+
+    dtf_tail = 5150
 
     cfg   = {
             'error_method' : 'minuit_hesse',
@@ -43,27 +45,42 @@ class Data:
         'jpsi_misid' : '/home/acampove/external_ssd/Data/samples/jpsi_misid.yaml'}
 # ------------------------------
 def _get_limits() -> tuple[int,int]:
-    if Data.rng == 'bcn':
+    if Data.kind == 'bcn':
         return 5050, 5600
 
-    if Data.rng == 'wide':
+    if Data.kind == 'wide':
         return 4500, 5600
 
-    if Data.rng == 'largest':
+    if Data.kind == 'largest':
         return 4500, 6000
 
-    raise ValueError(f'Invalid range: {Data.rng}')
+    if Data.kind == 'no_dtf_tail' and Data.mass == 'B_const_mass_M':
+        return Data.dtf_tail, 5600
+
+    if Data.kind == 'no_dtf_tail' and Data.mass == 'B_M':
+        return 4500, 6000
+
+    raise ValueError(f'Invalid kind: {Data.kind}')
+# ------------------------------
+def _get_cuts() -> dict[str,str]:
+    d_cut = {}
+    d_cut['nbrem'] = f'nbrem == {Data.nbrem}' if Data.nbrem in [0, 1] else f'nbrem >= {Data.nbrem}'
+
+    if Data.kind == 'no_dtf_tail':
+        d_cut[Data.kind] = f'B_const_mass_M > {Data.dtf_tail}'
+
+    return d_cut
 # ------------------------------
 def _parse_args() -> None:
     parser = argparse.ArgumentParser(description='Script used to fit resonant electron mode')
     parser.add_argument('-b', '--nbrem' , type=int, help='Brem category'   , required=True, choices=[0,1,2])
     parser.add_argument('-m', '--mass'  , type=str, help='Branch with mass', required=True, choices=['B_M', 'B_const_mass_M'])
-    parser.add_argument('-r', '--range' , type=str, help='Name of range'   , required=True, choices=['bcn', 'wide', 'largest'])
+    parser.add_argument('-k', '--kind'  , type=str, help='Type of fit'     , required=True, choices=['bcn', 'wide', 'largest', 'no_dtf_tail'])
     args = parser.parse_args()
 
     Data.nbrem = args.nbrem
     Data.mass  = args.mass
-    Data.rng   = args.range
+    Data.kind  = args.kind
 # ------------------------------
 def main():
     '''
@@ -73,16 +90,17 @@ def main():
 
     trigger = 'Hlt2RD_BuToKpEE_MVA'
     q2bin   = 'jpsi'
-
+    d_cut   = _get_cuts()
     t_lim   = _get_limits()
+
     obs     = zfit.Space(Data.mass, limits=t_lim)
 
     cmp_sig = cmp.get_mc(obs = obs, sample = 'Bu_JpsiK_ee_eq_DPC' , trigger=trigger, q2bin=q2bin, nbrem=Data.nbrem)
     cmp_csp = cmp.get_mc(obs = obs, sample = 'Bu_JpsiPi_ee_eq_DPC', trigger=trigger, q2bin=q2bin, nbrem=Data.nbrem)
-    cmp_prc = cmp.get_prc(obs= obs, trigger=trigger, q2bin=q2bin, nbrem=Data.nbrem)
+    cmp_prc = cmp.get_prc(obs= obs, trigger=trigger, q2bin=q2bin, name='no_tail', cuts = d_cut, bw = 20)
     cmp_cmb = cmp.get_cb(obs = obs, kind='exp')
 
-    rdf = cmp.get_rdf(sample='DATA*', q2bin=q2bin, trigger=trigger, nbrem=Data.nbrem)
+    rdf = cmp.get_rdf(sample='DATA*', q2bin=q2bin, trigger=trigger, cuts = d_cut)
 
     out_dir = Data.cfg['out_dir']
     Data.cfg['out_dir']= f'{out_dir}/nbrem_{Data.nbrem:03}'
