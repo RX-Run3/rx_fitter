279b3e582624f781a775126825528fece847c50f
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-27 13:07:55 +0800

    Will pick default model

--- src/rx_fitter_scripts/rx_reso_ee.py
+++ src/rx_fitter_scripts/rx_reso_ee.py
@@ -13,8 +13,8 @@ def main():
     obs=zfit.Space('B_const_mass_M', limits=(4500, 6000))
     trigger = 'Hlt2RD_BuToKpEE_MVA'
 
-    cmp_sig = cmp.get_mc(obs = obs, sample = 'Bu_JpsiK_ee_eq_DPC' , trigger, q2bin='jpsi', model=['cbl', 'cbl', 'cbr'])
-    cmp_csp = cmp.get_mc(obs = obs, sample = 'Bu_JpsiPi_ee_eq_DPC', trigger, q2bin='jpsi', model=['cbl', 'cbl', 'cbr'])
+    cmp_sig = cmp.get_mc(obs = obs, sample = 'Bu_JpsiK_ee_eq_DPC' , trigger, q2bin='jpsi') 
+    cmp_csp = cmp.get_mc(obs = obs, sample = 'Bu_JpsiPi_ee_eq_DPC', trigger, q2bin='jpsi')
     cmb_prc = cmp.get_prc(obs = obs, trigger=trigger, q2bin='jpsi')
     cmb_cmb = cmp.get_cb(kind='expo')
 # ------------------------------
