af3cd02ad59044e4a7f4826ff789ca269bd9e29e
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-28 21:35:10 +0800

    Updating with implementation

--- src/rx_fitter_scripts/reso_scale.py
+++ src/rx_fitter_scripts/reso_scale.py
@@ -6,9 +6,9 @@ import re
 import json
 import argparse
 
+import numpy
 import pandas            as pnd
 import matplotlib.pyplot as plt
-import pandas            as pnd
 
 from dmu.logging.log_store import LogStore
 from dmu.generic           import version_management as vman
@@ -88,13 +88,83 @@ def _get_df() -> pnd.DataFrame:
 
     return df
 #------------------------------------------
+def _prepare_df(df : pnd.DataFrame, kind : str) -> pnd.DataFrame:
+    df = df[df.kind == kind]
+    df = df.drop(columns=['kind'])
+    df = df.set_index('brem')
+
+    return df
+#------------------------------------------
+def _subtract_df(df_1, df_2) -> pnd.DataFrame:
+    sr_val  = df_1.Value - df_2.Value
+    sr_err  = numpy.sqrt(df_1.Error ** 2 - df_2.Error ** 2)
+    df      = pnd.DataFrame({'brem' : df_1.index, 'Value' : sr_val, 'Error' : sr_err})
+
+    return df
+#------------------------------------------
+def _divide_df(df_1, df_2) -> pnd.DataFrame:
+    v1 = df_1.Value
+    v2 = df_2.Value
+
+    e1 = df_1.Error
+    e2 = df_2.Error
+
+    vl = v1 / v2
+    er = vl * numpy.sqrt(e1/v1 ** 2 + e2/v2 ** 2)
+    df = pnd.DataFrame({'brem' : df_1.index, 'Value' : vl, 'Error' : er})
+
+    return df
+#------------------------------------------
+def _scale_from_df(df : pnd.DataFrame, parameter : str) -> pnd.DataFrame:
+    df_dt = _prepare_df(df, 'data')
+    df_mc = _prepare_df(df, 'mc'  )
+
+    if 'mu' in parameter:
+        df_sc = _subtract_df(df_dt, df_mc)
+    else:
+        df_sc = _divide_df(df_dt, df_mc)
+
+    return df_sc
+#------------------------------------------
+def _path_from_par(parameter : str) -> str:
+    if 'mu' in parameter:
+        return 'scale.png'
+
+    if 'sigma' in parameter and '1' in parameter:
+        return 'resolution_1.png'
+
+    if 'sigma' in parameter and '2' in parameter:
+        return 'resolution_2.png'
+
+    raise ValueError(f'Parameter not a scale or resolution: {parameter}')
+#------------------------------------------
+def _ylim_from_par(parameter: str) -> tuple[float,float]:
+    if 'mu' in parameter:
+        return -20, 20
+
+    return 0.5, 1.5
+#------------------------------------------
 def main():
     '''
     Starts here
     '''
 
     df = _get_df()
-    print(df)
+    for parameter, df_parameter in df.groupby('Parameter'):
+        df_parameter = df_parameter.drop(columns=['Parameter'])
+        df_scale = _scale_from_df(df_parameter, parameter)
+        df_scale.plot(x='brem', y='Value', yerr='Error')
+
+        plt.title(parameter)
+        plt.grid()
+        plt.legend([])
+
+        fig_path = _path_from_par(parameter)
+        t_ylim   = _ylim_from_par(parameter)
+
+        plt.ylim(t_ylim)
+
+        plt.savefig(fig_path)
 #------------------------------------------
 if __name__ == '__main__':
     main()
