afe9417b7b0fec03d2d013ca417d1570f537b3eb
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-02 17:23:11 +0800

    Moving configuration to YAML file

--- src/rx_fitter_scripts/rx_reso_ee.py
+++ src/rx_fitter_scripts/rx_reso_ee.py
@@ -4,15 +4,18 @@ Script used to fit the resonant mode in the electron channel
 import os
 import json
 import argparse
+from importlib.resources import files
 
+import yaml
 import ROOT
 import zfit
 
-from dmu.generic                             import version_management as vman
-from dmu.logging.log_store                   import LogStore
-from rx_calibration.hltcalibration.dt_fitter import DTFitter
-from rx_data.rdf_getter                      import RDFGetter
-from rx_fitter                               import components as cmp
+from dmu.generic                                 import version_management as vman
+from dmu.logging.log_store                       import LogStore
+from rx_calibration.hltcalibration.dt_fitter     import DTFitter
+from rx_calibration.hltcalibration.fit_component import FitComponent
+from rx_data.rdf_getter                          import RDFGetter
+from rx_fitter                                   import components as cmp
 
 log = LogStore.add_logger('rx_fitter:rx_reso_ee')
 # ------------------------------
@@ -22,69 +25,15 @@ class Data:
     '''
     nbrem : int
     mass  : str
-    kind  : str
     cpath : str
 
-    dtf_tail = 5160
-    trigger  = 'Hlt2RD_BuToKpEE_MVA'
-    q2bin    = 'jpsi'
-
-    cfg   = {
-            'error_method' : 'minuit_hesse',
-            'plotting'     :
-            {
-                'nbins'   : 30,
-                'stacked' : True,
-                'd_leg'   : {
-                    'Bu_JpsiK_ee_eq_DPC' : r'$B^+\to K^+J/\psi(\to e^+e^-)$',
-                    'Bu_JpsiPi_ee_eq_DPC': r'$B^+\to \pi^+J/\psi(\to e^+e^-)$',
-                    'combinatorial'      : 'Combinatorial',
-                    }
-                },
-            }
-
-    RDFGetter.samples = {
-        'main'       : '/home/acampove/external_ssd/Data/samples/main.yaml',
-        'mva'        : '/home/acampove/external_ssd/Data/samples/mva.yaml',
-        'hop'        : '/home/acampove/external_ssd/Data/samples/hop.yaml',
-        'cascade'    : '/home/acampove/external_ssd/Data/samples/cascade.yaml',
-        'jpsi_misid' : '/home/acampove/external_ssd/Data/samples/jpsi_misid.yaml'}
-# ------------------------------
-def _get_constraints() -> dict[str,str]:
-    if Data.cpath is None:
-        return None
-
-    with open(Data.cpath, encoding='utf-8') as ifile:
-        d_const = json.load(ifile)
-
-    d_const_yield = { name : value for name, value in d_const.items() if name.startswith('n') and name not in ['nPRec', 'ncombinatorial'] }
-
-    return d_const_yield
-# ------------------------------
-def _get_limits() -> tuple[int,int]:
-    if Data.kind == 'bcn':
-        return 5050, 5600
-
-    if Data.kind == 'wide':
-        return 4500, 5600
-
-    if Data.kind == 'largest':
-        return 4500, 6000
-
-    if Data.kind == 'no_dtf_tail' and Data.mass == 'B_const_mass_M':
-        return Data.dtf_tail, 5600
-
-    if Data.kind == 'no_dtf_tail' and Data.mass == 'B_M':
-        return 4500, 6000
-
-    raise ValueError(f'Invalid kind: {Data.kind}')
+    cfg   : dict
 # ------------------------------
 def _get_cuts() -> dict[str,str]:
-    d_cut = {}
-    d_cut['nbrem'] = f'nbrem == {Data.nbrem}' if Data.nbrem in [0, 1] else f'nbrem >= {Data.nbrem}'
-
-    if Data.kind == 'no_dtf_tail':
-        d_cut[Data.kind] = f'B_const_mass_M > {Data.dtf_tail}'
+    d_cut         = {}
+    d_cut['brem'] = Data.cfg['brem'][Data.nbrem]
+    d_sel         = Data.cfg['input']['selection']
+    d_cut.update(d_sel)
 
     return d_cut
 # ------------------------------
@@ -92,50 +41,92 @@ def _parse_args() -> None:
     parser = argparse.ArgumentParser(description='Script used to fit resonant electron mode')
     parser.add_argument('-b', '--nbrem' , type=int, help='Brem category'   , required=True, choices=[0,1,2])
     parser.add_argument('-m', '--mass'  , type=str, help='Branch with mass', required=True, choices=['B_M', 'B_const_mass_M'])
-    parser.add_argument('-k', '--kind'  , type=str, help='Type of fit'     , required=True, choices=['bcn', 'wide', 'largest', 'no_dtf_tail'])
     parser.add_argument('-c', '--cpath' , type=str, help='Path to JSON file with parameters to constraint')
     args = parser.parse_args()
 
     Data.nbrem = args.nbrem
     Data.mass  = args.mass
-    Data.kind  = args.kind
     Data.cpath = args.cpath
 # ------------------------------
 def _set_out_dir() -> None:
-    fit_dir = os.environ['FITDIR']
-    ver_dir = f'{fit_dir}/data/{Data.q2bin}'
+    q2bin   = Data.cfg['input']['q2bin'  ]
+    trigger = Data.cfg['input']['trigger']
+    fit_dir = Data.cfg['output']['fit_dir']
+
+    ver_dir = f'{fit_dir}/data/{q2bin}'
     ver_dir = vman.get_last_version(dir_path=ver_dir, version_only=False)
-    out_dir = f'{ver_dir}/DATA_{Data.trigger}/{Data.mass}_{Data.nbrem}/full_model'
+    out_dir = f'{ver_dir}/DATA_{trigger}/{Data.mass}_{Data.nbrem}/full_model'
 
     Data.cfg['out_dir'] = out_dir
+# ------------------------------
+def _load_config() -> None:
+    cfg_path = files('rx_fitter_data').joinpath('config/v1.yaml')
+    with open(cfg_path, encoding='utf-8') as ifile:
+        Data.cfg = yaml.safe_load(ifile)
+# ------------------------------
+def _get_limits() -> tuple[int,int]:
+    return Data.cfg['fitting']['range'][Data.mass]
+# ------------------------------
+def _fit_data(l_cmp : list[FitComponent]) -> None:
+    if not Data.cfg['fitting']['components']['data']:
+        log.info('Skipping fit to data')
+        return
+
+    q2bin   = Data.cfg['input']['q2bin']
+    trigger = Data.cfg['input']['trigger']
+    d_cut   = _get_cuts()
+
+    rdf     = cmp.get_rdf(sample='DATA*', q2bin=q2bin, trigger=trigger, cuts = d_cut)
+    d_const = {
+            'nPRec'                : [0, 1],
+            'nBu_JpsiPi_ee_eq_DPC' : [0, 1],
+            }
+
+    cfg_fit = Data.cfg['fitting']['config']['data']
+
+    _set_out_dir()
+    obj = DTFitter(rdf = rdf, components = l_cmp, cfg=cfg_fit)
+    obj.fit(constraints = d_const)
+# ------------------------------
+def _get_components() -> list[FitComponent]:
+    obs     = zfit.Space(Data.mass, limits=_get_limits())
+    q2bin   = Data.cfg['input']['q2bin']
+    trigger = Data.cfg['input']['trigger']
+    l_fcm   = []
+
+    if Data.cfg['fiting']['components']['Signal']:
+        sample = Data.cfg['fitting']['config']['Signal']['sample']
+        fcm    = cmp.get_mc(obs = obs, name= sample, trigger=trigger, q2bin=q2bin, nbrem=Data.nbrem)
+        l_fcm.append(fcm)
+
+    if Data.cfg['fiting']['components']['Cabibbo']:
+        sample = Data.cfg['fitting']['config']['Cabibbo']['sample']
+        fcm    = cmp.get_mc(obs = obs, name= sample, trigger=trigger, q2bin=q2bin, nbrem=Data.nbrem)
+        l_fcm.append(fcm)
+
+    if Data.cfg['fiting']['components']['combinatorial']:
+        kind  = Data.cfg['fitting']['config']['combinatorial']['kind']
+        fcm   = cmp.get_cb(obs = obs, kind= kind)
+        l_fcm.append(fcm)
+
+    if Data.cfg['fitting']['components']['PRec']:
+        d_cut = _get_cuts()
+        bw    = Data.cfg['fitting']['config']['PRec']['bw']
+        fcm   = cmp.get_prc(obs= obs, name= Data.nbrem, trigger=trigger, q2bin=q2bin, cuts = d_cut, bw = bw)
+        l_fcm.append(fcm)
+
+    return l_fcm
 # ------------------------------
 def main():
     '''
     Start here
     '''
     _parse_args()
+    _load_config()
 
-    d_cut   = _get_cuts()
-    t_lim   = _get_limits()
 
-    obs     = zfit.Space(Data.mass, limits=t_lim)
-
-    cmp_cmb = cmp.get_cb(obs = obs, kind='exp')
-    cmp_sig = cmp.get_mc(obs = obs, sample = 'Bu_JpsiK_ee_eq_DPC' , trigger=Data.trigger, q2bin=Data.q2bin, nbrem=Data.nbrem)
-    cmp_csp = cmp.get_mc(obs = obs, sample = 'Bu_JpsiPi_ee_eq_DPC', trigger=Data.trigger, q2bin=Data.q2bin, nbrem=Data.nbrem)
-    cmp_prc = cmp.get_prc(obs= obs, trigger=Data.trigger, q2bin=Data.q2bin, name=Data.nbrem, cuts = d_cut, bw = 20)
-
-    rdf = cmp.get_rdf(sample='DATA*', q2bin=Data.q2bin, trigger=Data.trigger, cuts = d_cut)
-
-    d_const = _get_constraints()
-    d_const = {
-            'nPRec'                : [0, 1],
-            'nBu_JpsiPi_ee_eq_DPC' : [0, 1],
-            }
-
-    _set_out_dir()
-    obj = DTFitter(rdf = rdf, components = [cmp_cmb, cmp_prc, cmp_csp, cmp_sig], cfg=Data.cfg)
-    obj.fit(constraints = d_const)
+    l_cmp   = _get_components()
+    _fit_data(l_cmp)
 # ------------------------------
 if __name__ == '__main__':
     main()
