27f8e504c8e0f447709cd06e9ff281d8579aba87
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-02 13:59:01 +0800

    Adding file

---
+++ tests/test_mc_par_pdf.py
@@ -0,0 +1,65 @@
+'''
+Module with tests for MCParPdf class
+'''
+import os
+import copy
+
+import ROOT
+import zfit
+from ROOT                 import RDataFrame
+from rx_data.rdf_getter   import RDFGetter
+from rx_fitter.mc_par_pdf import MCParPdf
+from rx_fitter            import components as cmp
+
+# ------------------------------------------
+class Data:
+    '''
+    data class
+    '''
+    fit_dir   = os.environ['FITDIR']
+    cache_dir = '/tmp/tests/rx_fits/mc_par_pdf'
+    cfg       = {
+            'fitting':
+            {
+                'error_method'  : 'minuit_hesse',
+                'weights_column': 'weights',
+                'ntries'        : 20,
+                'pvalue'        : 0.02,
+                },
+            'plotting' :
+            {
+                'nbins'   : 50,
+                'stacked' : True,
+                },
+            }
+
+    obs = zfit.Space('B_M', limits=[4500, 6000])
+# ------------------------------------------
+def _get_rdf(cfg : dict) -> RDataFrame:
+    nbrem   = cfg['nbrem']
+    cuts    = {'nbrem' : f'nbrem == {nbrem}'}
+    trigger = cfg['trigger']
+    q2bin   = cfg['q2bin']
+    sample  = cfg['name']
+
+    return cmp.get_rdf(sample, q2bin, trigger, cuts)
+# ------------------------------------------
+def test_simple():
+    '''
+    Simplest test of MCParPdf
+    '''
+    cfg            = copy.deepcopy(Data.cfg)
+    cfg['name'   ] = 'Bu_JpsiK_ee_eq_DPC'
+    cfg['q2bin'  ] = 'jpsi'
+    cfg['trigger'] = 'Hlt2RD_BuToKpEE_MVA'
+    cfg['nbrem'  ] = 1
+    cfg['fvers'  ] = None
+    cfg['shared' ] = ['mu']
+    cfg['model'  ] = ['suj', 'dscb']
+    cfg['pfloat' ] = ['mu', 'sg']
+
+    rdf   = _get_rdf(cfg=cfg)
+    obj   = MCParPdf(rdf=rdf, obs=Data.obs, cfg=cfg)
+
+    return obj.get_fcomp()
+# ------------------------------------------
