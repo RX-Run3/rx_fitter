0cd17e341f5e0f44005ee6a01b9acf50411218ca
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-11 18:57:31 +0800

    Adding logger and not hardcoding but picking from config

--- src/rx_fitter_scripts/model_tester.py
+++ src/rx_fitter_scripts/model_tester.py
@@ -7,24 +7,26 @@ from importlib.resources import files
 import ROOT
 import yaml
 import zfit
-from rx_data.rdf_getter import RDFGetter
-from rx_fitter          import components as cmp
+from dmu.logging.log_store import LogStore
+from rx_fitter             import components as cmp
 
+log = LogStore.add_logger('rx_fitter:model_tester')
 # --------------------------------
 class Data:
     '''
     data class
     '''
-    trigger = 'Hlt2RD_BuToKpEE_MVA'
-    sample  = 'Bu_JpsiK_ee_eq_DPC'
-    q2bin   = 'jpsi'
-
     cfg     : dict
 
     obs_name: str
     version : str
     nbrem   : int
     model   : str
+    level   : int
+# --------------------------------
+def _initialize():
+    LogStore.set_level('rx_fitter:components', Data.level)
+    _load_config()
 # --------------------------------
 def _load_config():
     cfg_path = files('rx_fitter_data').joinpath(f'model_tester/{Data.version}/reso_ee.yaml')
@@ -43,14 +45,16 @@ def _parse_args():
     parser = argparse.ArgumentParser(description='Script used to test fitting models')
     parser.add_argument('-m', '--model'  , type=str, help='Nickname of model'        , required=True)
     parser.add_argument('-v', '--vers'   , type=str, help='Version of configuration' , required=True)
-    parser.add_argument('-b', '--nbrem'  , type=int, help='Bremstrahlung category'   , required=True, choices=[-1, 0, 1, 2])
-    parser.add_argument('-o', '--obsname', type=str, help='Name of observable'       , required=True, choices=['B_M', 'B_const_mass_M'])
+    parser.add_argument('-b', '--nbrem'  , type=int, help='Bremstrahlung category'   , required=True , choices=[-1, 0, 1, 2])
+    parser.add_argument('-o', '--obsname', type=str, help='Name of observable'       , required=True , choices=['B_M', 'B_const_mass_M'])
+    parser.add_argument('-l', '--level'  , type=int, help='Logging level'            , default=20    , choices=[10, 20, 30])
     args = parser.parse_args()
 
     Data.model    = args.model
     Data.version  = args.vers
     Data.nbrem    = args.nbrem
     Data.obs_name = args.obsname
+    Data.level    = args.level
 # --------------------------------
 def main():
     '''
