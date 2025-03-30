9530692029f8a77d528109841afb2b82f26ebedc
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-30 22:30:26 +0800

    Adding file

---
+++ src/rx_fitter_scripts/validate_cmb.py
@@ -0,0 +1,10 @@
+'''
+Script used to validate PDFs needed to fit combinatorial
+'''
+
+
+# --------------------------------
+
+# --------------------------------
+if __name__ == '__main__':
+    main()
