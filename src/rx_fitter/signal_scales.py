d44be04c0529d32de38781dcdde7a33c7a2a4b99
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-09 23:14:10 +0800

    Adding file

---
+++ src/rx_fitter/signal_scales.py
@@ -0,0 +1,14 @@
+'''
+Module holding SignalScales class 
+'''
+
+# ------------------------------------
+class SignalScales:
+    '''
+    Class used to provide:
+
+    Mass scales
+    Mass resolutions
+    Brem "resolutions" 
+    '''
+# ------------------------------------
