b543903cd5ec1d29213b337dcb807ae5ed379602
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-09 17:14:07 +0800

    Will plot per q2 bin and store all the processes in dataframe

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ class ScalesData:
     data class
     '''
     df_def_wp = pnd.DataFrame(columns=['Process', 'Q2', 'Value', 'Error'])
-    df_mva_wp = pnd.DataFrame(columns=['mva_cut', 'Q2', 'Value', 'Error'])
+    df_mva_wp = pnd.DataFrame(columns=['Process', 'mva_cut', 'Q2', 'Value', 'Error'])
 
     plt.style.use(mplhep.style.LHCb2)
     # ------------------
@@ -28,12 +28,12 @@ class ScalesData:
         ScalesData.df_def_wp.loc[size] = [proc, q2bin, value, error]
     # ------------------
     @staticmethod
-    def collect_mva_wp(mvawp: str, q2bin : str, value : float, error : float) -> None:
+    def collect_mva_wp(proc : str, mvawp: str, q2bin : str, value : float, error : float) -> None:
         '''
         Picks test outputs and uses it to fill dataframe
         '''
-        size                          = len(ScalesData.df_mva_wp)
-        ScalesData.df_mva_wp.loc[size] = [mvawp, q2bin, value, error]
+        size                           = len(ScalesData.df_mva_wp)
+        ScalesData.df_mva_wp.loc[size] = [proc, mvawp, q2bin, value, error]
     # ------------------
     @staticmethod
     def plot_scales_def_wp():
@@ -58,32 +58,32 @@ class ScalesData:
         plt.close()
     # ------------------
     @staticmethod
-    def plot_scales_mva_wp():
+    def plot_scales_mva_wp(df_mva : pnd.DataFrame, q2bin : str):
         '''
         Plots scales from dataframe with scanned MVA WP
         '''
-        print(ScalesData.df_mva_wp)
-        df_mva            = ScalesData.df_mva_wp
         df_mva['mva_cut'] = df_mva.mva_cut.str.replace('mva_', '')
         df_mva['mva_cut'] = df_mva.mva_cut.str.replace('&&'  , '')
         df_mva['mva_cut'] = df_mva.mva_cut.str.replace('('   , '')
         df_mva['mva_cut'] = df_mva.mva_cut.str.replace(')'   , '')
 
         plt.figure(figsize=(30,20))
-        for q2bin, df in df_mva.groupby('Q2'):
-            plt.plot(df.mva_cut, df.Value, label=q2bin, color='blue')
-            plt.fill_between(df.mva_cut, df.Value - df.Error, df.Value + df.Error, color='blue', alpha=0.2)
+        for process, df in df_mva.groupby('Process'):
+            decay = dn.tex_from_decay(process)
+            plt.plot(df.mva_cut, df.Value, label=decay)
+            plt.fill_between(df.mva_cut, df.Value - df.Error, df.Value + df.Error, alpha=0.2)
 
         out_dir = 'plots/prec_scales'
         os.makedirs(out_dir, exist_ok=True)
 
         plt.legend()
+        plt.title(q2bin)
         plt.grid()
-        plt.ylim(0.0, 0.4)
+        plt.ylim(0.0, 0.60)
         plt.xlabel('')
         plt.xticks(rotation=70)
         plt.ylabel(r'$N_{PRec}/N_{Signal}$')
-        plt.savefig(f'{out_dir}/scales_mva_wp.png')
+        plt.savefig(f'{out_dir}/scales_mva_wp_{q2bin}.png')
         plt.close()
 # -----------------------------------
 def pytest_sessionfinish():
@@ -91,5 +91,7 @@ def pytest_sessionfinish():
     Runs at the end
     '''
     ScalesData.plot_scales_def_wp()
-    ScalesData.plot_scales_mva_wp()
+
+    for q2bin, df_q2 in ScalesData.df_mva_wp.groupby('Q2'):
+        ScalesData.plot_scales_mva_wp(df_mva = df_q2, q2bin=q2bin)
 # -----------------------------------
