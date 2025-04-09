6463cc8071a7c453bac0d5f02f6793b108d97f8d
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-09 18:43:09 +0800

    Will cache data and load it

--- src/rx_fitter_scripts/rx_rare_ee.py
+++ src/rx_fitter_scripts/rx_rare_ee.py
@@ -1,17 +1,22 @@
 '''
 Script used to fit the rare mode
 '''
+import os
 from importlib.resources import files
 
 import yaml
 import ROOT
+import numpy
 import zfit
 from zfit.core.data         import Data       as zdata
 from zfit.core.basepdf      import BasePDF    as zpdf
 from zfit.core.interfaces   import ZfitSpace  as zobs
 from zfit.core.parameter    import Parameter  as zpar
+from dmu.generic            import hashing
+from dmu.generic            import utilities  as gut
 from dmu.stats.fitter       import Fitter
 from dmu.logging.log_store  import LogStore
+from dmu.stats.utilities    import print_pdf
 from rx_data.rdf_getter     import RDFGetter
 from rx_selection           import selection  as sel
 from rx_fitter              import components as cmp
@@ -22,6 +27,8 @@ class Data:
     '''
     Data class
     '''
+    cache_dir: str = '/tmp/rx_fitter/cache'
+    mva_cut : str  = '(mva_cmb > 0.8) && (mva_prc > 0.8)'
     sample  : str  = 'DATA*'
     trigger : str  = 'Hlt2RD_BuToKpEE_MVA'
     version : str  = 'v1'
@@ -31,6 +38,7 @@ class Data:
     maxx    : int  = 6_000
     obs     : zobs = zfit.Space(mass, limits=(minx, maxx))
     nsig    : zpar = zfit.Parameter('nsig', 0, 0, 10_000)
+    gut.TIMER_ON   = True
 
     l_pdf   : list[zpdf] = []
 # --------------------------------------------------------------
@@ -52,6 +60,7 @@ def _add_pdf_cmb() -> None:
 def _add_pdf_prc(sample : str) -> None:
     cfg                   = _load_config(component='bxhsee')
     cfg['input']['q2bin'] = Data.q2bin
+    cfg['selection']      = {'mva' : Data.mva_cut}
 
     pdf  = cmp.get_kde(obs=Data.obs, sample=sample, nbrem=None, cfg=cfg)
     scale= zfit.Parameter(f's{sample}', 0, 0, 10)
@@ -63,9 +72,10 @@ def _add_pdf_prc(sample : str) -> None:
 def _add_pdf_leak(sample : str) -> None:
     cfg                   = _load_config(component='ccbar_leak')
     cfg['input']['q2bin'] = Data.q2bin
+    cfg['selection']      = {'mva' : Data.mva_cut}
 
     pdf   = cmp.get_kde(obs=Data.obs, sample=sample, nbrem=None, cfg=cfg)
-    nleak = zfit.Parameter(f'n{sample}', 0, 0, 10_000) 
+    nleak = zfit.Parameter(f'n{sample}', 0, 0, 10_000)
     pdf.set_yield(nleak)
 
     Data.l_pdf.append(pdf)
@@ -79,6 +89,7 @@ def _add_pdf_sig() -> None:
 
     Data.l_pdf.append(pdf)
 # --------------------------
+@gut.timeit
 def _get_pdf() -> zpdf:
     _add_pdf_cmb()
     _add_pdf_sig()
@@ -95,13 +106,32 @@ def _get_pdf() -> zpdf:
 
     return pdf
 # --------------------------
+@gut.timeit
 def _get_data() -> zdata:
     gtr = RDFGetter(sample=Data.sample, trigger=Data.trigger)
     rdf = gtr.get_rdf()
-    rdf = sel.apply_full_selection(rdf=rdf, project='RK', trigger=Data.trigger, q2bin=Data.q2bin, process=Data.sample)
+
+    d_sel        = sel.selection(project='RK', trigger=Data.trigger, q2bin=Data.q2bin, process=Data.sample)
+    d_sel['mva'] = Data.mva_cut
+
+    hsh             = hashing.hash_object([d_sel, Data.sample, Data.trigger])
+    data_cache_path = f'{Data.cache_dir}/{hsh}.json'
+    if os.path.isfile(data_cache_path):
+        log.warning(f'Caching data from: {data_cache_path}')
+        l_mass   = gut.load_json(data_cache_path)
+        arr_mass = numpy.ndarray(l_mass)
+        data = zfit.Data.from_numpy(obs=Data.obs, array=arr_mass)
+
+        return data
+
+    for cut_name, cut_expr in d_sel.items():
+        rdf = rdf.Filter(cut_expr, cut_name)
 
     arr_mass = rdf.AsNumpy([Data.mass])[Data.mass]
-    data     = zfit.Data.from_numpy(obs=Data.obs, array=arr_mass)
+    log.info(f'Caching data to: {data_cache_path}')
+    gut.dump_json(arr_mass.tolist(), data_cache_path)
+
+    data = zfit.Data.from_numpy(obs=Data.obs, array=arr_mass)
 
     return data
 # --------------------------
