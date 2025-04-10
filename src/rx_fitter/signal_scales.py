2d39ca608eee5670f57fbe83e1b54c103ce4587b
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-10 13:02:56 +0800

    Adding code from reso_scale script

--- src/rx_fitter/signal_scales.py
+++ src/rx_fitter/signal_scales.py
@@ -1,7 +1,17 @@
 '''
-Module holding SignalScales class 
+Module holding SignalScales class
 '''
+import os
+import glob
+import json
+import numpy
+import jacobi
+import pandas as pnd
 
+from dmu.generic           import version_management as vman
+from dmu.logging.log_store import LogStore
+
+log = LogStore.add_logger('rx_fitter:signal_scales')
 # ------------------------------------
 class SignalScales:
     '''
@@ -9,6 +19,135 @@ class SignalScales:
 
     Mass scales
     Mass resolutions
-    Brem "resolutions" 
+    Brem "resolutions"
     '''
+    # -----------------------------------
+    def __init__(self):
+        '''
+        Initializer
+        '''
+        self._l_brem    = [0, 1, 2]
+        self._l_kind    = ['data', 'mc']
+        self._fit_dir   = os.environ['FITDIR']
+
+        self._mc_sample = 'Signal'
+        self._trigger   = 'Hlt2RD_BuToKpEE_MVA'
+        self._mass      = 'B_M_brem_track_2'
+    #------------------------------------------
+    def _name_from_parname(self, name : str) -> str:
+        if name.startswith('mu_'):
+            return r'$\mu$'
+
+        if name.startswith('sg_'):
+            return r'$\sigma$'
+
+        if name.startswith('nSignal'):
+            return 'yield'
+
+        if name.startswith('nl_'):
+            return 'nl'
+
+        if name.startswith('nr_'):
+            return 'nr'
+
+        if name.startswith('al_'):
+            return 'al'
+
+        if name.startswith('ar_'):
+            return 'ar'
+
+        return None
+    #------------------------------------------
+    def _df_from_pars(self, d_par : dict[str,list[str]]) -> pnd.DataFrame:
+        d_data = {'Parameter' : [], 'Value' : [], 'Error' : []}
+        for name, [val, err] in d_par.items():
+            name = self._name_from_parname(name)
+            if name is None:
+                continue
+
+            d_data['Parameter'].append(name)
+            d_data['Value'    ].append(val)
+            d_data['Error'    ].append(err)
+
+        df = pnd.DataFrame(d_data)
+
+        return df
+    # -----------------------------------
+    def _get_df_fit(self, kind : str, brem : int) -> pnd.DataFrame:
+        sample   = self._mc_sample if kind == 'mc' else 'DATA'
+
+        inp_path = f'{self._fit_dir}/{kind}/jpsi'
+        inp_path = vman.get_last_version(dir_path=inp_path, version_only=False)
+        inp_wc   = f'{inp_path}/{sample}_{self._trigger}/{self._mass}_{brem}/*/parameters.json'
+        l_path   = glob.glob(inp_wc)
+        npath    = len(l_path)
+        if npath != 1:
+            raise ValueError(f'No one and only one path found in {inp_wc}')
+
+        log.debug(f'Looking for parameters in: {inp_path}')
+        with open(l_path[0], encoding='utf-8') as ifile:
+            d_par = json.load(ifile)
+
+        df = self._df_from_pars(d_par)
+
+        log.debug(f'Parameters for kind {kind}, brem {brem}')
+        log.debug(df)
+
+        return df
+    # -----------------------------------
+    def _frac_from_yield(self, df : pnd.DataFrame) -> pnd.DataFrame:
+        l_df = []
+        for _, df_kind in df.groupby('kind'):
+            df          = df_kind[df_kind.Parameter == 'yield']
+            frac, cov   = jacobi.propagate(lambda x : x / numpy.sum(x), df.Value.values, df.Error.values)
+            df['Value'] = frac
+            df['Error'] = numpy.sqrt(numpy.diag(cov))
+            df.Parameter= df.Parameter.replace({'yield' : 'frac'})
+
+            for index in df.index:
+                df_kind.loc[index] = df.loc[index]
+
+            l_df.append(df_kind)
+
+        df = pnd.concat(l_df, axis=0)
+
+        return df
+    #------------------------------------------
+    def _pick_common_parameters(self, df_dt : pnd.DataFrame, df_mc : pnd.DataFrame) -> tuple[pnd.DataFrame, pnd.DataFrame]:
+        l_par_dt = df_dt.Parameter.unique().tolist()
+        l_par_mc = df_mc.Parameter.unique().tolist()
+        l_common = [ par_dt for par_dt in l_par_dt if par_dt in l_par_mc ]
+
+        df_dt = df_dt[df_dt.Parameter.isin(l_common)]
+        df_mc = df_mc[df_mc.Parameter.isin(l_common)]
+
+        return df_dt, df_mc
+    # -----------------------------------
+    def get_data(self) -> pnd.DataFrame:
+        '''
+        Returns dataframe with parameter values from fits to data and MC
+        '''
+        l_df_brem = []
+        for brem in self._l_brem:
+            l_df_kind = []
+            for kind in self._l_kind:
+                log.debug(f'Extracting parameters for {kind}/{brem}')
+                df         = self._get_df_fit(kind = kind, brem = brem)
+                df['kind'] = kind
+
+                l_df_kind.append(df)
+
+            [df_dt, df_mc] = l_df_kind
+            [df_dt, df_mc] = self._pick_common_parameters(df_dt, df_mc)
+
+            df = pnd.concat([df_dt, df_mc], axis=0)
+            df['brem'] = brem
+
+            l_df_brem.append(df)
+
+        df = pnd.concat(l_df_brem, axis=0)
+        df = df.reset_index(drop=True)
+        df = self._frac_from_yield(df)
+
+        return df
 # ------------------------------------
