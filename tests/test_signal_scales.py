ee1f6d860b82d2c108949a64e18a8be883e51693
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-10 12:43:13 +0800

    Adding file

---
+++ tests/test_signal_scales.py
@@ -0,0 +1,23 @@
+'''
+Module with functions testing SignalScales class
+'''
+
+import pytest
+from dmu.logging.log_store   import LogStore
+from rx_fitter.signal_scales import SignalScales
+
+log=LogStore.add_logger('rx_fitter:test_signal_scales')
+# --------------------------------------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _intialize():
+    LogStore.set_level('rx_fitter:signal_scales', 10)
+# ------------------------------------
+def test_get_data():
+    '''
+    Tests getting dataframe with parameters
+    '''
+    obj = SignalScales()
+    df  = obj.get_data()
+
+    print(df)
+# ------------------------------------
