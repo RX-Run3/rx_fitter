942e7a10af4336fdb248284d065a431b139947c0
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-24 18:11:22 +0800

    Add tabulate_pdfs

---
+++ src/rx_fitter_scripts/tabulate_pdfs.py
@@ -0,0 +1,13 @@
+'''
+Script used to make tex files from txt files corresponding to zfit PDFs
+'''
+
+
+# --------------------------------
+def main():
+    '''
+    Start here
+    '''
+# --------------------------------
+if __name__ == '__main__':
+    main()
