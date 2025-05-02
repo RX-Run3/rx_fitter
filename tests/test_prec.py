7c9d64baec44832c4cb0964c24724905e4120393
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-02 18:39:22 +0800

    Remove lines with bandwidth

--- tests/test_prec.py
+++ tests/test_prec.py
@@ -83,7 +83,6 @@ def test_reso(q2bin : str):
     trig   = 'Hlt2RD_BuToKpEE_MVA'
     mass   = {'jpsi' : 'B_const_mass_M', 'psi2' : 'B_const_mass_psi2S_M'}[q2bin]
     maxy   = {'jpsi' : 10_000          , 'psi2' :                  2_000}[q2bin]
-    bw     = {'jpsi' :  5              , 'psi2' :                     10}[q2bin]
     l_samp = [
             'Bu_JpsiX_ee_eq_JpsiInAcc',
             'Bd_JpsiX_ee_eq_JpsiInAcc',
@@ -127,7 +126,6 @@ def test_bdt(q2bin : str, bdt_cut : str, name : str):
     trig   = 'Hlt2RD_BuToKpEE_MVA'
     mass   = {'jpsi' : 'B_const_mass_M', 'psi2' : 'B_const_mass_psi2S_M'}[q2bin]
     maxy   = {'jpsi' : 20_000          , 'psi2' :                  4_000}[q2bin]
-    bw     = {'jpsi' :  5              , 'psi2' :                     10}[q2bin]
     l_samp = [
             'Bu_JpsiX_ee_eq_JpsiInAcc',
             'Bd_JpsiX_ee_eq_JpsiInAcc',
@@ -154,7 +152,6 @@ def test_brem(brem_cut : str, name : str):
     obs=zfit.Space('mass', limits=(4500, 6000))
     trig   = 'Hlt2RD_BuToKpEE_MVA'
     mass   = {'jpsi' : 'B_const_mass_M', 'psi2' : 'B_const_mass_psi2S_M'}[q2bin]
-    bw     = {'jpsi' :  5              , 'psi2' :                     10}[q2bin]
     l_samp = [
             'Bu_JpsiX_ee_eq_JpsiInAcc',
             'Bd_JpsiX_ee_eq_JpsiInAcc',
@@ -182,7 +179,6 @@ def test_cache():
     trig   = 'Hlt2RD_BuToKpEE_MVA'
     mass   = {'jpsi' : 'B_const_mass_M', 'psi2' : 'B_const_mass_psi2S_M'}[q2bin]
     maxy   = {'jpsi' : 20_000          , 'psi2' :                  4_000}[q2bin]
-    bw     = {'jpsi' :  5              , 'psi2' :                     10}[q2bin]
     l_samp = [
             'Bu_JpsiX_ee_eq_JpsiInAcc',
             'Bd_JpsiX_ee_eq_JpsiInAcc',
