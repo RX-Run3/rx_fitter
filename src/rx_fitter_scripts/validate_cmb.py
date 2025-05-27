04e3f4a8b267ba2772271e0f9eb03b7c581f1fc5
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-27 19:45:51 +0800

    Bug: q2_kind will always be defined

--- src/rx_fitter_scripts/validate_cmb.py
+++ src/rx_fitter_scripts/validate_cmb.py
@@ -125,10 +125,10 @@ def _get_out_dir() -> str:
     ana_dir = os.environ['ANADIR']
     out_dir = Data.cfg['output']['path']
 
-    if hasattr(Data, 'q2_kind'):
-        out_dir = f'{ana_dir}/{Data.q2_kind}/{out_dir}'
-    else:
+    if Data.q2_kind is None:
         out_dir = f'{ana_dir}/{out_dir}'
+    else:
+        out_dir = f'{ana_dir}/{Data.q2_kind}/{out_dir}'
 
     os.makedirs(out_dir, exist_ok=True)
 
