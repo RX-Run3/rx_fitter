4ce820f5af327aeadb6fff0ec02d0efd4c0e776c
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-05 21:00:21 +0800

    Adding central bin

--- src/rx_fitter/prec.py
+++ src/rx_fitter/prec.py
@@ -184,7 +184,7 @@ class PRec:
             d_match = self._get_match_str_jpsi()
         elif self._q2bin == 'psi2':
             d_match = self._get_match_str_psi2()
-        elif self._q2bin == 'high':
+        elif self._q2bin in ['central', 'high']:
             d_match = self._get_match_str_psi2()
         else:
             raise ValueError(f'Invalid q2bin: {self._q2bin}')
